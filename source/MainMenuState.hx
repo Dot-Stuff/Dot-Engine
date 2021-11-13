32485412874f2613aed28dd626d98e9320577b57
AmberHaxe <86689764+AmberHaxe@users.noreply.github.com>
2021-11-13 15:09:13 -0500

    fixes 0.035 to 0.06 lol

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -215,7 +215,7 @@ class MainMenuState extends MusicBeatState
 
 	override function update(elapsed:Float)
 	{
-		FlxG.camera.followLerp = CoolUtil.camLerpShit(0.035);
+		FlxG.camera.followLerp = CoolUtil.camLerpShit(0.06);
 
 		if (FlxG.sound.music.volume < 0.8)
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
