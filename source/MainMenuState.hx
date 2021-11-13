87797a0fcbda595807dc2eaab73f5e8176ba50fb
AmberHaxe <86689764+AmberHaxe@users.noreply.github.com>
2021-11-12 22:32:39 -0500

    Changes cameraFollow's lerp to match to 2.8
    Changes the int value in CameraFollow's lerp to match Vanilla 2.8 on the main menu

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -215,7 +215,7 @@ class MainMenuState extends MusicBeatState
 
 	override function update(elapsed:Float)
 	{
-		FlxG.camera.followLerp = CoolUtil.camLerpShit(0.6);
+		FlxG.camera.followLerp = CoolUtil.camLerpShit(0.035);
 
 		if (FlxG.sound.music.volume < 0.8)
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
